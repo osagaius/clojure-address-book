5220998ae1d85ede68b11e583e40420eacc6e827
Osayame Gaius-Obaseki <osayame@gmail.com>
2015-05-21 22:31:39 -0400

    Added new function for get route to serve hiccup template. Restructured code to use separate routes, views, and resources folders

--- src/address_book/core/handler.clj
+++ src/address_book/core/handler.clj
@@ -1,16 +1,20 @@
 (ns address-book.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [address-book.core.routes.address-book-routes :refer [address-book-routes]]))
+
+
+(defn init []
+  (println "Address book application is starting"))
 
 (defn example-post [request]
   (let [post-value (get-in request [:params :example-post])]
     (str "You posted: " post-value)))
 
 (defroutes app-routes
-  (GET "/" [] "Example GET")
-  (POST "/post" [] example-post)
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false)))
+  (-> (routes address-book-routes app-routes)
+      (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))))
